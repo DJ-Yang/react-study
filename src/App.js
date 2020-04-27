0d7a26235e4bada40be299b35fa238d441b7f79b
DJ-Yang <dong3831@hanmail.net>
2020-04-27 22:30:31 +0900

    react study chapter 7

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MyComponent from './MyComponent';
-import EventPractice from './EventPractice';
-import Counter from './Counter';
-import Say from './Say';
-import ValidationSample from './ValidationSample';
-import ScrollBox from './ScrollBox';
-import IterationSample from './IterationSample';
+// import MyComponent from './MyComponent';
+// import EventPractice from './EventPractice';
+// import Counter from './Counter';
+// import Say from './Say';
+// import ValidationSample from './ValidationSample';
+// import ScrollBox from './ScrollBox';
+// import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
+
+// 랜덤 색상 생성 함수
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000',
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
     return (
-      <IterationSample />
+      // 라이플 사이클 실습
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+
+      // 컴포넌트 반복 실습
+      // <IterationSample />
 
       // 클래스 기반 DOM ref 사용 실습
       // <ValidationSample />
